e2b6903104ab1192c8b54dc804e895910e78d696
buituan11 <buituan71099@gmail.com>
2019-05-11 19:06:13 +0700

    fix loi update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,51 +7,59 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      Time: null,
-      tzone: 'UTC',
-      msg: 'now',
+      current: false,
+      time: ''
     }
   } 
-  shouldComponentUpdate(nextProps, nextState) {
-    return this.state.Time !== nextState.Time ||
-           this.state.tzone !== nextState.tzone ||
-           this.state.msg !== nextState;
-  }
-  fetchTime = () => {
-    let api = 'https://andthetimeis.com' + '/'+ this.state.tzone + '/' + this.state.msg + '.json';
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return this.state.Time !== nextState.Time ||
+  //          this.state.tzone !== nextState.tzone ||
+  //          this.state.msg !== nextState;
+  // }
+  fetchTime = (tzone, msg) => {
+    let api = 'https://andthetimeis.com' + '/'+ tzone + '/' + msg + '.json';
     console.log( api );
     fetch( api )
     .then( resp => resp.json() )
     .then( data => {
         const Time = data.dateString;
-        this.setState({ Time: Time });
+        this.setState({ time: Time });
     })
   }
+  initTime = () =>{
+    this.setState({ current: true });
+    this.fetchTime('UTC', 'now');
+  }
   changeUpdate = ( newUpdate ) => {
-    console.log(newUpdate.tzone);
+    // console.log( this.state.time );
     if ( newUpdate.tzone && newUpdate.msg){
-      this.setState({ tzone: newUpdate.tzone, msg: newUpdate.msg});
-      this.fetchTime();
+      this.setState({ time: this.fetchTime(newUpdate.tzone, newUpdate.msg) });
     }
   }
   showTime = () => {
     return ( 
       <div>
-        <p> { this.state.Time } </p>
+        <p> { this.state.time } </p>
         <TimeForm
           update = { (newUpdate) => this.changeUpdate( newUpdate) }
         />
-      </div> 
+      </div>            
     );
   }
 
   render() {
-    let Time = this.state.Time;
     return( 
       <div className="App">
         <h1>Time API</h1> 
-        { !Time && <button onClick={ () => this.fetchTime() }> Current Time in UTC </button> }
-        { Time && this.showTime() }
+        { !this.state.current && <button onClick={ () => { this.initTime() } }> Current Time in UTC </button> }
+        { this.state.current && this.showTime() }
+        <ul>
+          <li> /utc/now → Current UTC time </li>
+          <li> /pdt/in+two+hours → Time two hours from now in PDT </li>
+          <li> /pdt/next+monday → Datetime for next Monday in PDT </li>
+          <li> /pdt/this+friday+at+noon → Datetime for this Friday at noon in PDT ("friday noon" works too) </li>
+          <li> /pdt/7+hours+before+tomorrow+at+noon → I think you get the point </li>
+        </ul>
       </div>
     );
   };
